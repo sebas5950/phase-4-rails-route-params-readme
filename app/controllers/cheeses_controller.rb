38786f99c742a3c6d96f2d284bcbb66bff179927
Alex Guanipatin <sebas5950@gmail.com>
2022-07-18 21:09:42 -0400

    commit tree 1c4bb7a522825358a8193ed04a8721c71ce826d8

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,8 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
 end
